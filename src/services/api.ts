3e7b98c724ed415782ae3b502df117dc974048a4
Garrett Van Neste <garrett.van.neste@MacBookPro.home>
2025-01-08 21:11:54 -0800

    Removed console.log

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,9 +2,7 @@ import { Product } from "../types/Product.ts";
 
 export const fetchData = async (): Promise<Product[]> => {
   try {
-    console.log("HERE");
     const response = await fetch("/stackline/data/mockData.json");
-    console.log({ response });
     const data = await response.json();
     return data;
   } catch (error) {
